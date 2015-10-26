d6c5ff735d71b7acdba19c1b715b40c068b17d10
Tony Guyot <tguyot@yahoo.com>
2015-10-26 16:37:06 +0100

    First version of UI for query panel

--- app/src/main/java/tonyguyot/github/io/acronym/QueryFragment.java
+++ app/src/main/java/tonyguyot/github/io/acronym/QueryFragment.java
@@ -5,12 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class QueryFragment extends Fragment {
+
+    TextView mTvQuery;
 
     public QueryFragment() {
         // Required empty public constructor
@@ -25,6 +29,27 @@ public class QueryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_query, container, false);
+        View view = inflater.inflate(R.layout.fragment_query, container, false);
+
+        // retrieve the different UI items we need to interact with
+        mTvQuery = (TextView) view.findViewById(R.id.query_entry);
+
+        // define the callback for the button
+        Button submitButton = (Button) view.findViewById(R.id.query_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick();
+            }
+        });
+
+        return view;
+    }
+
+    // ------ CALLBACKS ------
+
+    // called when the user clicks on the submit button
+    private void onButtonClick() {
+        Utils.toast(getContext(), mTvQuery.getText().toString());
     }
 }
